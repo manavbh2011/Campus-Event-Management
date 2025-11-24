a2ccb8f820600f08beb6e1daa8871b807b60d04b
vennasaur <102436564+vennasaur@users.noreply.github.com>
2025-11-23 20:47:25 -0500

    Change database connection settings
    Updated database connection credentials for new environment.

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 class Database {
-    private $host = 'localhost';
-    private $db_name = 'wtm6hs';
-    private $username = 'wtm6hs';
-    private $password = 'xTb7GT1RRuTh';
+    private $host = 'db_2025';//'localhost';
+    private $db_name = 'example';//'wtm6hs';
+    private $username = 'localuser';//'wtm6hs';
+    private $password = 'cs4640LocalUser!';//'xTb7GT1RRuTh';
     private $port = 5432;
     private $conn;
 
